df8b7bb677710c0c6b2a48c5f3cc77a8735613bc
kreezxil <kreezxil@gmail.com>
2020-08-24 16:01:49 -0500

    3.8.15
    Seems I disabled the sound handler when I first ported this. That is fixed now. The mod now also silences the wander trader too.

--- src/main/java/com/kreezcraft/badwithernocookiereloaded/BadWitherNoCookie.java
+++ src/main/java/com/kreezcraft/badwithernocookiereloaded/BadWitherNoCookie.java
@@ -1,18 +1,15 @@
 package com.kreezcraft.badwithernocookiereloaded;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod("bwncr")
 public class BadWitherNoCookie {
@@ -21,25 +18,25 @@ public class BadWitherNoCookie {
 
 	public static BadWitherNoCookie instance;
 
-	public static  boolean whatWasThat = false;
 	public static PlayerEntity player;
 	
-	//public static SideProxy proxy = DistExecutor.runForDist(() -> SideProxy.Client::new, () -> SideProxy.Server::new);
+	public static SideProxy proxy = DistExecutor.runForDist(() -> SideProxy.Client::new, () -> SideProxy.Server::new);
 
 	public BadWitherNoCookie() {
 
-		/*ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BWNCR_Config.spec);
+		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BWNCR_Config.spec);
 		
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(BadWitherNoCookie::clientSetup);*/
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(BadWitherNoCookie::clientSetup);
 		
 		MinecraftForge.EVENT_BUS.register(this);
 		instance = this;
 	}
 
-	/*private static void clientSetup(FMLClientSetupEvent event) {
+	private static void clientSetup(FMLClientSetupEvent event) {
     	proxy.clientSetup(event);
     }
-	
+
+    /*
 	@SubscribeEvent
 	public void serverStarting(FMLServerStartingEvent event) {
 		//proxy.serverStarting(event);
--- src/main/java/com/kreezcraft/badwithernocookiereloaded/ListenCommand.java
+++ src/main/java/com/kreezcraft/badwithernocookiereloaded/ListenCommand.java
@@ -10,7 +10,7 @@ public final class ListenCommand {
 
 	private ListenCommand() {}
 	
-	public static void register(CommandDispatcher<CommandSource> dispatcher) {
+	/*public static void register(CommandDispatcher<CommandSource> dispatcher) {
 		dispatcher.register(Commands.literal("listen")
 				.requires(source -> source.hasPermissionLevel(2) || source.getServer().isSinglePlayer())
 				.executes((command) -> {
@@ -20,5 +20,5 @@ public final class ListenCommand {
 					command.getSource().sendFeedback(new StringTextComponent("Event Listening is now " + (BadWitherNoCookie.whatWasThat ? "on":"off")),true);
 					return 0;
 				}));
-	}
+	}*/
 }
